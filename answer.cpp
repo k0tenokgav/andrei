69d311395e7dae0fa3e57bb780d7c35508527c36
k0tenokgav <134811779+k0tenokgav@users.noreply.github.com>
2023-05-28 00:25:28 +0300

    Create answer.cpp

---
+++ answer.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+    int i;
+    int no = 0;
+    int* bot;
+    bot = new int[n];
+    for (i=1; i <= n; i++) {
+        cin >> bot[i];
+    }
+    for (i=2; i<=n; i++){
+        if (bot[i] < bot[i - 1])
+            no = 1;
+    }
+    
+    if (no == 1) {
+        cout << "-1";
+    }
+    else {
+        int max=1;
+        int schet = 0;
+        for (i = 1;i <= n;i++) {
+            if (bot[i] > max)
+                max = bot[i];
+        }
+        while (bot[1] != bot[n]) {
+            bot[1]++;
+            schet++;
+        }
+        cout << schet;
+    }
+    return 0;
+}
